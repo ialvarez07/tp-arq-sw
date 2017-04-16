1dfd58cb86ecf79c39d8de2fbf5d184437d6871e
Facu Parra <facundo.parra.94@gmail.com>
2017-04-16 17:57:55 -0300

    Agregada funcionalidad grafica a la Aplicacion

--- Applicacion/appTempASW/src/main/java/appTemp/Application.java
+++ Applicacion/appTempASW/src/main/java/appTemp/Application.java
@@ -1,15 +1,41 @@
 package appTemp;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.Timer;
+import org.jfree.ui.RefineryUtilities;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.client.RestTemplate;
 
-public class Application {
+public class Application implements ActionListener{
     private static final Logger log = LoggerFactory.getLogger(Application.class);
     
+    private final Timer timer = new Timer(5000, this);
+    
     public static void main(String[] args) {
+        Grafica demo = Grafica.getInstance();
+        demo.pack();
+        RefineryUtilities.centerFrameOnScreen(demo);
+        demo.setVisible(true);
+        
+        new Application();
+    }
+
+    public Application() {
+        timer.setInitialDelay(100000);
+        timer.start();
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
         RestTemplate restTemplate = new RestTemplate();
         Medicion medicion = restTemplate.getForObject("http://192.168.0.200/temperatura", Medicion.class);
+        
         log.info(medicion.toString());
+        
+        Grafica.getInstance().addData(medicion);
     }
+    
+    
 }
--- Applicacion/appTempASW/src/main/java/appTemp/Grafica.java
+++ Applicacion/appTempASW/src/main/java/appTemp/Grafica.java
@@ -1,5 +1,76 @@
 package appTemp;
 
-public class Grafica {
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.ValueAxis;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.data.time.Millisecond;
+import org.jfree.data.time.TimeSeries;
+import org.jfree.data.time.TimeSeriesCollection;
+import org.jfree.data.xy.XYDataset;
+import org.jfree.ui.ApplicationFrame;
+
+public class Grafica extends ApplicationFrame {
+    private static Grafica instancia = null;
     
+    private final TimeSeries datos;
+    
+    private Grafica (String chartTitle, String dataTitle, String xTitle, String yTitle) {
+        super(chartTitle);
+        
+        this.datos = new TimeSeries(dataTitle, xTitle, yTitle);
+        
+        TimeSeriesCollection dataset = new TimeSeriesCollection(this.datos);
+        
+        JFreeChart chart = createChart(dataset, dataTitle, xTitle, yTitle);
+        chart.setBackgroundPaint(Color.LIGHT_GRAY);
+        
+        JPanel content = new JPanel(new BorderLayout());
+        ChartPanel chartPanel = new ChartPanel(chart);
+        content.add(chartPanel);
+        chartPanel.setPreferredSize(new java.awt.Dimension(800, 500));
+        setContentPane(content);
+    }
+ 
+    private JFreeChart createChart(XYDataset dataset, String dataTitle, String xTitle, String yTitle) {
+        JFreeChart result = ChartFactory.createTimeSeriesChart(dataTitle, xTitle, yTitle, dataset, true, true, false);
+ 
+        XYPlot plot = result.getXYPlot();
+
+        plot.setBackgroundPaint(new Color(0xffffe0));
+        plot.setDomainGridlinesVisible(true);
+        plot.setDomainGridlinePaint(Color.LIGHT_GRAY);
+        plot.setRangeGridlinesVisible(true);
+        plot.setRangeGridlinePaint(Color.LIGHT_GRAY);
+
+        ValueAxis xaxis = plot.getDomainAxis();
+        xaxis.setAutoRange(true);
+        //xaxis.setFixedAutoRange(60000.0);
+        xaxis.setVerticalTickLabels(true);
+
+        ValueAxis yaxis = plot.getRangeAxis();
+        yaxis.setRange(0.0, 40.0);
+
+        return result;
+    }
+    
+    public void addData(Medicion medicion) {
+        Time t = medicion.getTime();
+        Millisecond ms = new Millisecond(0, t.getSegundos(), t.getMinutos(), t.getHora(), t.getDia(), t.getMes(), 2017);
+        this.datos.add(ms, medicion.getTemperatura());
+    }
+   
+    public static Grafica getInstance() {
+        if (instancia == null) {
+            instancia = new Grafica("titiitititulo", "Mediciones", "Tiempo", "Temperatura");
+        }
+        return instancia;
+    }
 }
